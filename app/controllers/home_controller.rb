c3ec3ee73b989f2830a3691e38ca3eab548d37f5
tjy86 <thomasjyang@gmail.com>
2013-08-19 16:12:02 -0400

    nav_link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,7 @@ class HomeController < ApplicationController
   def new
   end
 
+  def story
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Bd::Application.routes.draw do
   root :to => 'home#new'
+  get 'story' => 'home#story'
 end
