15ad2db3b039ffcce5e572678a3bc70074da41c0
tjy86 <thomasjyang@gmail.com>
2013-08-20 17:04:47 -0400

    nav 90%

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,14 @@ module ApplicationHelper
   def nav_link(link_text, link_path)
     class_name = current_page?(link_path) ? 'active' : ''
 
-    content_tag(:li, :class => class_name) do
+    content_tag(:li, :class => class_name, :class => 'nav-li' ) do
+      link_to link_text, link_path
+    end
+  end
+  def nav_link_small(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name, :class => 'nav-li-small' ) do
       link_to link_text, link_path
     end
   end
