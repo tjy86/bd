103992afc1ce7a16f069e36e683db3983a5ca749
tjy86 <thomasjyang@gmail.com>
2013-08-29 13:07:04 -0400

    fixed gmt time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,15 @@
 class HomeController < ApplicationController
 
   def new
-    @now = Time.now
-    start_of_day = @now.at_midnight
-    @eightam = start_of_day + 28800 # 8am = 8 * 60 * 60
-    @onepm = start_of_day + 46800 # 1pm = 13 * 60 * 60
-
+    Time.use_zone('GMT') do
+      now = Time.zone.now - 14400 # GMT less four hours = NY time
+      start_of_day = now.at_midnight
+      eightam = start_of_day + 28800 # 8am = 8 * 60 * 60
+      onepm = start_of_day + 46800 # 1pm = 13 * 60 * 60
+      @e = eightam
+      @o = onepm
+      @n = now
+    end
   end
   def story
   end
--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,17 +1,19 @@
 class OrderController < ApplicationController
 
   def pickup
-    Time.use_zone('UTC') do
-      now = Time.now
+    Time.use_zone('GMT') do
+      now = Time.zone.now - 14400 # GMT less four hours = NY time
       start_of_day = now.at_midnight
-      @eightam = start_of_day + 28800 # 8am = 8 * 60 * 60
-      @onepm = start_of_day + 46800 # 1pm = 13 * 60 * 60
-
-    #   if now > eightam && now < onepm && Date.today.cwday >= 1 && Date.today.cwday <= 5
-    #     render "pickup"
-    #   else
-    #     render "closed"
-    #   end
+      eightam = start_of_day + 28800 # 8am = 8 * 60 * 60
+      onepm = start_of_day + 46800 # 1pm = 13 * 60 * 60
+      @e = eightam
+      @o = onepm
+      @n = now
+      if now > eightam && now < onepm && Date.today.cwday >= 1 && Date.today.cwday <= 5
+        render "pickup"
+      else
+        render "closed"
+      end
     end
   end
   def soldout
