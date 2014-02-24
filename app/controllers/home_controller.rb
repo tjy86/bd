d6a70a97f8935b304b5e056f67c2e3f43996ca1a
tjy86 <thomasjyang@gmail.com>
2014-02-24 12:11:49 -0500

    update time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
       now = Time.zone.now - 14400 # GMT less four hours = NY time
       start_of_day = now.at_midnight
       eightam = start_of_day + 28800 # 8am = 8 * 60 * 60
-      onepm = start_of_day + 46800 # 1pm = 13 * 60 * 60
+      onepm = start_of_day + 52200 # 1pm = 13 * 60 * 60
       @e = eightam
       @o = onepm
       @n = now
